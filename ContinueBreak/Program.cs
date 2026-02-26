d7cccbd30c7deea5106a06410a6bfd381de6ffa0
Mohan Lal <mhnuk2007@gmail.com>
2026-02-26 16:34:18 +0500

    Add break and continue examples

---
+++ ContinueBreak/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] nums = new[]{15, 7, 12, 23, 41, 28, 9, 17, 32};
+
+// break
+foreach(int num in nums)
+{
+    Console.WriteLine(num);
+    if(num == 23)
+    {
+        break;
+    }
+}
+Console.WriteLine("Done!");
+
+// continue
+foreach(int num in nums)
+{
+    if(num % 2 == 0)
+    {
+        continue;
+    }
+    Console.WriteLine(num);
+}
+Console.WriteLine("Done!"); 
