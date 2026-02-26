66b1822f6221420b793e635ad08dc603bc0fa0d9
Mohan Lal <mhnuk2007@gmail.com>
2026-02-27 00:25:10 +0500

    Add solution to string code challenge

---
+++ StringCodeChallenge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class StringCodeChallenge
+{
+    public static bool countTheType(object Arg, string TypeToCount)
+    {
+        string argTypeName = Arg.GetType().Name;
+        return argTypeName == TypeToCount;
+    
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(countTheType(123, "Int32"));
+        Console.WriteLine(countTheType("Hello", "String"));
+        Console.WriteLine(countTheType(true, "Boolean"));
+    }
+}
+    
