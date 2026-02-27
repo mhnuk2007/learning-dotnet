62d5fb2e7a24dddfd18d8c19490204084491e229
Mohan Lal <mhnuk2007@gmail.com>
2026-02-27 16:40:56 +0500

    Add function basics and function params examples

---
+++ FunctionBasics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class FunctionBasics
+{
+    // Convert miles to kilometers
+    static double MilesToKm(double Miles)
+    {
+        return Miles * 1.60934;
+    }
+
+    // print string with prefix
+    static void PrintWithPrefix(string thestr)
+    {
+        Console.WriteLine($"::> {thestr}");
+    }
+    public static void Main()
+    {
+        // Calls
+        Console.WriteLine($"The result is {MilesToKm(8.0):F2}");
+        Console.WriteLine($"The result is {MilesToKm(52.0):F2}");
+
+        PrintWithPrefix("Test string");
+        PrintWithPrefix("Another test string");
+    }
+}
---
+++ FunctionParams/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Program
+{
+    static void PrintWithPrefix(string thestr, string prefix = "")
+    {
+        Console.WriteLine($"{prefix} {thestr}");
+    }
+    public static void Main()
+    {
+        PrintWithPrefix("Hello Honey!");
+        PrintWithPrefix("Hello Honey", ">:");
+
+        // call with named params
+        PrintWithPrefix(prefix: ">:", thestr: "Hello Honey");
+    }
+}
